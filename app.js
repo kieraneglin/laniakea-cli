8eb3653b57cc7d711306125d47050fa1729f28c1
Kieran Eglin <kieran.eglin@gmail.com>
2017-03-01 00:43:10 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const Laniakea = require('laniakea');
+l = new Laniakea('/Users/Kieran/Documents/Programming/rom-renamer/laniakea/test_roms/dest');
+l.renameFile('/Users/Kieran/Documents/Programming/rom-renamer/laniakea/test_roms/source/1943.nes');
